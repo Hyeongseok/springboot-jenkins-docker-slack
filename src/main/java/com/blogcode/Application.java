eca0ed0ac947e592db0089707b0addbfa3acb781
Hyeongseok <romanze76@hotmail.com>
2018-09-18 13:30:51 +0200

    remove comment

--- src/main/java/com/blogcode/Application.java
+++ src/main/java/com/blogcode/Application.java
@@ -3,7 +3,6 @@ package com.blogcode;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-/* Added comment */
 @SpringBootApplication
 public class Application {
 
